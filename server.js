585825c26f2034c9216aafc779ad99043825cd7e
Evaldas <surauciusevaldas@gmail.com>
2019-04-16 22:10:00 +0300

    Change default server ip address

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/process', function (req, res) {
 console.log(process.env);
 
 const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.listen(server_port,server_ip_address, function() {
   console.log(`Listening on ${server_ip_address}:${server_port}`)
