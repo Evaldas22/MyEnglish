4fb4361a3d51db12d36b8cc278ce63bbac6d853f
Evaldas <surauciusevaldas@gmail.com>
2019-04-16 21:56:13 +0300

    Add console log

--- server.js
+++ server.js
@@ -21,9 +21,11 @@ app.get('/process', function (req, res) {
   res.json(process.env);
 });
 
-const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+console.log(process.env);
 
-app.listen(server_port,server_ip_address, () => {
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.listen(server_port,server_ip_address, function() {
   console.log(`Listening on ${server_ip_address}:${server_port}`)
 });
