e9c6eec9a90b3817ea2183f1b1cd1a2a857d609d
Evaldas <surauciusevaldas@gmail.com>
2019-04-29 19:42:33 +0300

    Add missing date in logger.error()

--- logging/logger.js
+++ logging/logger.js
@@ -5,6 +5,8 @@ const logger = {
     console.info(`[INFO] ${formatedDate} - ${message}`);
   },
   error: (message) => {
+    const today = new Date();
+    const formatedDate = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')} ${today.getHours()}:${today.getMinutes()}:${today.getMilliseconds()}`;
     console.error(`[ERROR] ${formatedDate} - ${message}`);
   }
 }
