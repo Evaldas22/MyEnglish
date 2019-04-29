f28f79a7431d9387e442ec3babdccdfc064c6cd7
Evaldas <surauciusevaldas@gmail.com>
2019-04-29 18:04:40 +0300

    Add groupName to logging info

--- routes/api/groups.js
+++ routes/api/groups.js
@@ -17,7 +17,7 @@ router.get('/groups', (req, res) => {
 // @desc    Get all students for certain group
 // @access  Public
 router.get('/group', (req, res) => {
-	logger.info("GET api/group/{groupName} - requesting data about specific group");
+	logger.info(`GET api/group/${groupName} - requesting data about specific group`);
 	GroupModel.find({ name: req.query.groupName })
 		.then(group => res.json(group))
 });
