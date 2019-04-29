295d4932545b98b9240f33f3c8f4b604e2e4f853
Evaldas <surauciusevaldas@gmail.com>
2019-04-29 18:02:44 +0300

    Create custom logger

---
+++ logging/logger.js
@@ -0,0 +1,12 @@
+const logger = {
+  info: (message) => {
+    const today = new Date();
+    const formatedDate = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')} ${today.getHours()}:${today.getMinutes()}:${today.getMilliseconds()}`;
+    console.info(`[INFO] ${formatedDate} - ${message}`);
+  },
+  error: (message) => {
+    console.error(`[ERROR] ${formatedDate} - ${message}`);
+  }
+}
+
+module.exports = logger;
