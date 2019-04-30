a0e2b1a4c2dd58a8663f5627c9b414bf21c95c01
Evaldas <surauciusevaldas@gmail.com>
2019-04-30 18:15:50 +0300

    Use moment.js instead of custom date formatting

--- logging/logger.js
+++ logging/logger.js
@@ -1,17 +1,11 @@
-/* 
- * in OpenShift time is GMT+0, so we add +3 (for debugging in Lithuania time [EEST])
-*/
+const moment = require('moment');
 
 const logger = {
   info: (message) => {
-    const today = new Date();
-    const formatedDate = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')} ${today.getHours()+3}:${today.getMinutes()}:${today.getMilliseconds()}`;
-    console.info(`[INFO] ${formatedDate} - ${message}`);
+    console.info(`[INFO] [${moment(Date.now()).utcOffset('+0300').format('YYYY-MM-DD h:mm:ss a')}]  ${message}`);
   },
   error: (message) => {
-    const today = new Date();
-    const formatedDate = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')} ${today.getHours()+3}:${today.getMinutes()}:${today.getMilliseconds()}`;
-    console.error(`[ERROR] ${formatedDate} - ${message}`);
+    console.error(`[ERROR] [${moment(Date.now()).utcOffset('+0300').format('YYYY-MM-DD h:mm:ss a')}]  ${message}`);
   }
 }
 
