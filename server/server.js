68b49a6d10c6306fcb4ba62e7f7938c780be1b71
Evaldas <surauciusevaldas@gmail.com>
2019-05-01 20:38:03 +0300

    Add testing console.log to see __dirname

--- server/server.js
+++ server/server.js
@@ -65,6 +65,8 @@ if (process.env.NODE_ENV === "production") {
   logger.info("Application is running in production");
   app.use(express.static('client/build'));
 
+  logger.info("Directory: ", __dirname);
+
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
