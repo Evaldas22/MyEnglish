f9161a69eef2fd6c49958078d0b2f4332056ae79
Evaldas <surauciusevaldas@gmail.com>
2019-05-03 20:05:47 +0300

    Add initial reducers and empty actions

--- client/src/App.js
+++ client/src/App.js
@@ -7,17 +7,22 @@ import Landing from "./components/common/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 class App extends Component {
   render(){
     return (
-      <BrowserRouter>
-        <div className="App">
-          <NavBar />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <NavBar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
