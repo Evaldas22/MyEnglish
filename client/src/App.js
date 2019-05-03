5d5bf6d7ebfc81e407cd079f426f1a7aacbc27aa
Evaldas <surauciusevaldas@gmail.com>
2019-05-03 19:19:22 +0300

    Add initial front-end for loging in and registration

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      This works!
-    </div>
-  );
+import NavBar from "./components/common/NavBar";
+import Landing from "./components/common/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+
+class App extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <NavBar />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
