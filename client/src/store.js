ea8b9e5c0bcca28dc7a4e3a4bbfe257cf76a4aee
Evaldas <surauciusevaldas@gmail.com>
2019-05-04 18:44:21 +0300

    Remove dev tools from production

--- client/src/store.js
+++ client/src/store.js
@@ -9,11 +9,12 @@ if (process.env.NODE_ENV !== 'production') {
   middleware = [...middleware, logger];
 }
 
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware)
   )
 );
 export default store;
