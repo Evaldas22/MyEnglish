1a746c4b977006b908b734e8c12ccf7dc515da9e
Evaldas <surauciusevaldas@gmail.com>
2019-05-06 18:11:30 +0300

    Change routes to be in order

--- server/routes/api/groups.js
+++ server/routes/api/groups.js
@@ -4,21 +4,21 @@ const logger = require('../../logging/logger');
 
 var GroupModel = require('../../models/Group').GroupModel;
 
-// @route   GET api/groups
-// @desc    Get all groups
-// @access  Public
-router.get('/groups', (req, res) => {
-	logger.info("GET api/groups");
-	GroupModel.find()
-		.then(group => res.json(group))
-});
-
 // @route   GET api/groups/{teacherId}
 // @desc    Get all students for one teacher
 // @access  Public
 router.get('/groups', (req, res) => {
 	logger.info(`GET api/groups/${req.query.teacherId} `);
 	GroupModel.find({ teacherId: req.query.teacherId })
+		.then(group => res.json(group))
+});
+
+// @route   GET api/groups
+// @desc    Get all groups
+// @access  Public
+router.get('/groups', (req, res) => {
+	logger.info("GET api/groups");
+	GroupModel.find()
 		.then(group => res.json(group))
 });
 
