ab7e7e63c2567d8737d6e31eadc9bc0b829de282
Evaldas <surauciusevaldas@gmail.com>
2019-05-10 17:00:38 +0300

    Refactor new word endpoint to add parse and add word with translation

--- server/routes/api/words.js
+++ server/routes/api/words.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const getStudent = require('./students').getStudent;
-const getNewWords = require('./students').getNewWords;
-const getWordsArrayFromString = require('./students').getWordsArrayFromString;
 var _ = require('lodash');
 var unirest = require('unirest');
 var apiKey = process.env.apiKey;
@@ -10,6 +8,7 @@ const url = require('url');
 const logger = require('../../logging/logger');
 
 var GroupModel = require('../../models/Group').GroupModel;
+const WordModel = require('../../models/Word').WordModel;
 
 // @route   GET api/word/{messengerId}{groupName}
 // @desc    Get one word for revision
@@ -19,25 +18,25 @@ router.get('/word', (req, res) => {
 	const messengerId = query['messenger user id'];
 	const groupName = query['groupName'];
 	logger.info(`GET api/word for  ${messengerId}`);
-	
+
 	GroupModel.findOne({ groupName: groupName }).then(group => {
-			const student = getStudent(group, messengerId);
-		
-			if (!student) {
-				logger.error(`student with ${messengerId} not found`);
-				return res.status(500).json('Student not found');
-			}
-			else if (student.knownWords.length <= 0) {
-				logger.error(`student with ${messengerId} doesn't have any knownWords`);
-				return res.status(404).json("Student doesn't know any words");
-			}
+		const student = getStudent(group, messengerId);
 
-			const wordForRevision = getWordForRevision(student.knownWords);
-			
-			const response = constructResponse(wordForRevision);
+		if (!student) {
+			logger.error(`student with ${messengerId} not found`);
+			return res.status(500).json('Student not found');
+		}
+		else if (student.knownWords.length <= 0) {
+			logger.error(`student with ${messengerId} doesn't have any knownWords`);
+			return res.status(404).json("Student doesn't know any words");
+		}
 
-			res.json(response);
-		})
+		const wordForRevision = getWordForRevision(student.knownWords);
+
+		const response = constructResponse(wordForRevision);
+
+		res.json(response);
+	})
 });
 
 // @route   POST api/word/update/{messengerId}{groupName}{word}{knowIt}
@@ -55,37 +54,37 @@ router.post('/word/update', (req, res) => {
 
 	GroupModel.findOne({ groupName: groupName }).then(group => {
 
-			if (_.isUndefined(group)) {
-				logger.error(`group - ${group} does not exist`);
-				return res.status(404).json('Group not found');
+		if (_.isUndefined(group)) {
+			logger.error(`group - ${group} does not exist`);
+			return res.status(404).json('Group not found');
+		}
+
+		const student = getStudent(group, messengerId);
+
+		if (_.isUndefined(student)) {
+			logger.error(`student - ${messengerId} does not exist`);
+			return res.status(500).json('Student not found');
+		}
+		else if (student.knownWords.length <= 0) {
+			logger.error(`student - ${messengerId} does not have any known words`);
+			return res.status(404).json("Student doesn't know any words");
+		}
+
+		student.knownWords.forEach(knownWord => {
+			if (knownWord.word === word) {
+				knownWord.score += (knowIt === "true") ? 1 : 0;
 			}
+		});
 
-			const student = getStudent(group, messengerId);
-
-			if (_.isUndefined(student)) {
-				logger.error(`student - ${messengerId} does not exist`);
-				return res.status(500).json('Student not found');
+		group.save(err => {
+			if (err) {
+				logger.error(`Error saving student(${messengerId}[${groupName}]) data`);
+				logger.error(err);
+				return res.status(500).json(err);
 			}
-			else if (student.knownWords.length <= 0) {
-				logger.error(`student - ${messengerId} does not have any known words`);
-				return res.status(404).json("Student doesn't know any words");
-			}
-
-			student.knownWords.forEach(knownWord => {
-				if (knownWord.word === word) {
-					knownWord.score += (knowIt === "true") ? 1 : 0;
-				}
-			}); 
-
-			group.save(err => {
-				if (err) {
-					logger.error(`Error saving student(${messengerId}[${groupName}]) data`);
-					logger.error(err);
-					return res.status(500).json(err);
-				}
-				res.json(student);
-			});
-		})
+			res.json(student);
+		});
+	})
 });
 
 // @route   POST api/word/newWords/{messengerId}{groupName}{newWords}
@@ -100,29 +99,29 @@ router.post('/word/newWords', (req, res) => {
 
 	GroupModel.findOne({ groupName: groupName }).then(group => {
 
-			if (_.isUndefined(group)) {
-				logger.error(`group ${groupName} doesn not exist`);
-				return res.status(404).json('Group not found');
+		if (_.isUndefined(group)) {
+			logger.error(`group ${groupName} doesn not exist`);
+			return res.status(404).json('Group not found');
+		}
+
+		const student = getStudent(group, messengerId);
+		if (_.isUndefined(student)) {
+			logger.error(`student ${messengerId} doesn not exist`);
+			return res.status(404).json('Student not found');
+		}
+
+		const newWordsToBeAdded = getNewWordsWithTranslation(student.knownWords, getWordsWithTranslationArrayFromString(newWords));
+		student.knownWords.push.apply(student.knownWords, newWordsToBeAdded);
+
+		group.save(err => {
+			if (err) {
+				logger.error(`Error saving student(${messengerId}[${groupName}]) data`);
+				logger.error(err);
+				return res.status(500).json(err);
 			}
-
-			const student = getStudent(group, messengerId);
-			if (_.isUndefined(student)) {
-				logger.error(`student ${messengerId} doesn not exist`);
-				return res.status(404).json('Student not found');
-			} 
-
-			const newWordsToBeAdded = getNewWords(student.knownWords, getWordsArrayFromString(newWords));
-			student.knownWords.push.apply(student.knownWords, newWordsToBeAdded);
-
-			group.save(err => {
-				if (err) {
-					logger.error(`Error saving student(${messengerId}[${groupName}]) data`);
-					logger.error(err);
-					return res.status(500).json(err);
-				}
-				res.json(student);
-			});
-		})
+			res.json(student);
+		});
+	})
 });
 
 // @route   GET api/word/definition/{word}
@@ -201,19 +200,19 @@ function getRandomWord(words) {
 const constructResponse = word => {
 	return {
 		"set_attributes":
-			{
-				"revisionWord": word
-			}
+		{
+			"revisionWord": word
+		}
 	}
 }
 
 const constructDefinitionResponse = definitionsObj => {
 	if (_.isEmpty(definitionsObj.definitions)) {
-		return { "messages": [ {"text": "We couldn't find any definition for this word :/"}	]	};
+		return { "messages": [{ "text": "We couldn't find any definition for this word :/" }] };
 	}
 	else {
 		const differentDefinitionsObjs = [];
-		
+
 		definitionsObj.definitions.forEach(definition => {
 			if (!partOfSpeechExists(differentDefinitionsObjs, definition.partOfSpeech)) {
 				differentDefinitionsObjs.push(definition);
@@ -231,7 +230,7 @@ const constructDefinitionResponse = definitionsObj => {
 			}
 		});
 
-		return { "messages": textMessages	};
+		return { "messages": textMessages };
 	}
 }
 
@@ -249,17 +248,76 @@ const partOfSpeechExists = (differentDefinitionsObjs, partOfSpeech) => {
 
 const constructExampleResponse = (exampleObj, exampleNumber) => {
 	if (_.isEmpty(exampleObj.examples)) {
-		return { "messages": [ {"text": "We couldn't find any examples for this word :/"} ] };
+		return { "messages": [{ "text": "We couldn't find any examples for this word :/" }] };
 	}
 	else {
 		if (exampleNumber > exampleObj.examples.length) {
-			return { "messages": [ {"text": "Sorry, but I don't have any examples left..."} ]	}
+			return { "messages": [{ "text": "Sorry, but I don't have any examples left..." }] }
 		}
-		else{
+		else {
 			const example = exampleObj.examples[exampleNumber - 1];
-			return { "messages": [ {"text": example} ] };
+			return { "messages": [{ "text": example }] };
 		}
 	}
+}
+
+const getWordsWithTranslationArrayFromString = (wordsString) => {
+	return wordsString.split(/,\s*/);
+}
+
+const getNewWordsWithTranslation = (knownWords, newWordsWithTranslationArr) => {
+	const newWordsToBeAdded = [];
+
+	newWordsWithTranslationArr.map(newWordWithTranslation => {
+		const newWord = getWordOrTranslation(newWordWithTranslation, true);
+		const newWordTranslation = getWordOrTranslation(newWordWithTranslation, false);
+
+		// if we have translation 
+		if (!_.isUndefined(newWordTranslation)) {
+			// only add that new word if it doesn't already exist in DB and in new words array (we don't want duplicates)
+			if (!alreadyExisitsInCollection(knownWords, newWord) && !alreadyExisitsInCollection(newWordsToBeAdded, newWord)) {
+				newWordsToBeAdded.push(new WordModel({
+					word: newWord,
+					score: 0,
+					translation: newWordTranslation
+				}));
+			}
+		}
+		else {
+			// check google translate API for it
+			const translationFromAPI = "TRANSLATION";
+
+			if (translationFromAPI) {
+				if (!alreadyExisitsInCollection(knownWords, newWord) && !alreadyExisitsInCollection(newWordsToBeAdded, newWord)) {
+					newWordsToBeAdded.push(new WordModel({
+						word: newWord,
+						score: 0,
+						translation: translationFromAPI
+					}));
+				}
+			}
+		}
+	})
+
+	return newWordsToBeAdded;
+}
+
+const getWordOrTranslation = (wordWithTranslation, returnWord) => {
+	const pair = wordWithTranslation.split(/\s*-\s*/);
+
+	if (returnWord) {
+		return pair[0];
+	}
+	if (pair.length > 1) {
+		return pair[1];
+	}
+
+	return undefined;
+}
+
+// collection - it needs to be an array of our Word Models
+const alreadyExisitsInCollection = (collection, word) => {
+	return collection.filter(knownWord => (knownWord.word === word)).length > 0;
 }
 
 module.exports = router;
