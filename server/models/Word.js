dd64006a33a54791ba8b7c9dd39b5aae666058f9
Evaldas <surauciusevaldas@gmail.com>
2019-05-10 20:58:11 +0300

    Add frequency to Word model

--- server/models/Word.js
+++ server/models/Word.js
@@ -2,12 +2,17 @@ const mongoose = require('mongoose');
 
 // Score will mark how many times student knew this word
 // +1 for knowing
+// Frequency will mark how many times word was used in revision
 const WordSchema = new mongoose.Schema({
     word: {
         type: String,
         required: true
     },
     score: {
+        type: Number,
+        required: true
+    },
+    frequency: {
         type: Number,
         required: true
     },
